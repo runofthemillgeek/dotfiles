09d17c51402ff4659fb55037af37f346e15db2cb
Sangeeth Sudheer <git@sangeeth.dev>
2024-04-07 16:52:43 +0530

    add gpg config

--- install-or-update.mjs
+++ install-or-update.mjs
@@ -6,3 +6,6 @@ await $`cp tmux/tmux.conf ~/.tmux.conf`;
 await echo(chalk.blue("Syncing git config"))
 await $`cp git/.gitconfig ~/.gitconfig`;
 
+await echo(chalk.blue("Syncing gpg config"))
+await $`cp gpg/gpg-agent.conf ~/.gnupg/gpg-agent.conf`
+
