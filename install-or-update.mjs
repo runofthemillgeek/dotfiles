c370867af20f9dd0ede9226eae089675f3dd4305
Sangeeth Sudheer <git@sangeeth.dev>
2024-04-07 16:49:45 +0530

    add .gitconfig

--- install-or-update.mjs
+++ install-or-update.mjs
@@ -2,3 +2,7 @@
 
 await echo(chalk.blue("Syncing tmux settings"))
 await $`cp tmux/tmux.conf ~/.tmux.conf`;
+
+await echo(chalk.blue("Syncing git config"))
+await $`cp git/.gitconfig ~/.gitconfig`;
+
